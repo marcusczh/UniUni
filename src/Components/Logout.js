1b6767df74a22e1945c334d9516466328c1a86cf
Ssatu <69383469+Ssatu@users.noreply.github.com>
2022-05-28 10:20:04 +0800

    Removing old src files

--- src/Components/Logout.js
+++
@@ -1,9 +0,0 @@
-import styles from "./HomePage.module.css";
-
-function logOut() {
-    return (
-        <button className={styles.logOut}>Welcome, User</button>
-    );
-}
-
-export default logOut;
